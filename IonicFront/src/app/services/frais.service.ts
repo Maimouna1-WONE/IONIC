d3b98db60f3b9c663da93d568581c329a3d975db
Maimouna WONE <mainashou@gmail.com>
2021-03-13 16:06:28 +0000

    13

--- IonicFront/src/app/services/frais.service.ts
+++ IonicFront/src/app/services/frais.service.ts
@@ -4,7 +4,7 @@ import {Injectable} from '@angular/core';
   providedIn: 'root'
 })
 export class FraisService{
-  page: string; frais: number;
+  frais: number;
   constructor() { }
 
   calculerFrais(montant: number){
